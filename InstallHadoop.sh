64035e79537e3315b76bab2d06f9dcbe125474a3
Ashesh Kumar Singh <user501254@gmail.com>
2019-10-23 14:05:47 -0500

    Change `HADOOP_OPTS` path

--- InstallHadoop.sh
+++ InstallHadoop.sh
@@ -198,7 +198,7 @@ export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export YARN_HOME=$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 #HADOOP VARIABLES END
 EOT
